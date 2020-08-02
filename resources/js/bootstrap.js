9466ebfb1446d59598db47fd552e03bf56130e11
charleyCC <249662192@qq.com>
2020-08-03 00:53:25 +0800

    创建后台登录、退出、首页模块

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,10 @@ try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
 
+
     require('bootstrap');
+    require('datatables.net-bs4');
+
 } catch (e) {}
 
 /**
