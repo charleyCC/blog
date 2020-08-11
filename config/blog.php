f137de92be25d5d582897e773e8f4daa2b96b7aa
charleychan <249662192@qq.com>
2020-08-11 14:22:00 +0800

    完善文章管理模块，美化前端界面

--- config/blog.php
+++ config/blog.php
@@ -1,9 +1,14 @@
 <?php
 return [
-        'title' => 'My Blog',
-        'posts_per_page' => 5,
-        'uploads' => [
-            'storage' => 'public',
-            'webpath' => '/storage',
-        ],
+    'name' => "Laravel 学习",
+    'title' => "Laravel 学习",
+    'subtitle' => 'http://myblog.cc',
+    'description' => 'Laravel学院致力于提供优质Laravel中文学习资源',
+    'author' => '博客',
+    'page_image' => 'home-bg.jpg',
+    'posts_per_page' => 10,
+    'uploads' => [
+        'storage' => 'public',
+        'webpath' => '/storage/uploads',
+    ],
 ];
